e27dc1cacb9d0191ef5d8643e8b7d5587d8fef59
Kota Kawamoto <atok.reverse.1040@gmail.com>
2025-08-04 16:02:57 +0900

    feat(items): refactor item model to use Prisma types and update create method to interact with the database

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Item } from './items.model';
+import { Item, ItemStatus } from 'generated/prisma';
 import { CreateItemDto } from './dto/create-item.dto';
-import { v4 as uuid } from 'uuid';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ItemsService {
+  constructor(private readonly prismaService: PrismaService) {}
   private items: Item[] = [];
   findAll(): Item[] {
     return this.items;
@@ -16,15 +17,19 @@ export class ItemsService {
     }
     return found;
   }
-  create(createItemDto: CreateItemDto): Item {
-    const item: Item = {
-      id: uuid(),
-      ...createItemDto,
-      status: 'ON_SALE',
-    };
-    this.items.push(item);
-    return item;
+
+  async create(createItemDto: CreateItemDto): Promise<Item> {
+    const { name, price, description } = createItemDto;
+    return this.prismaService.item.create({
+      data: {
+        name,
+        price,
+        description,
+        status: ItemStatus.ON_SALE,
+      },
+    });
   }
+
   updateStatus(id: string): Item | undefined {
     const item = this.findById(id);
     if (item) {

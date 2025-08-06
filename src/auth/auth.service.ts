6704e01ef6aec18ac7adf1243fb4626033637489
Kota Kawamoto <atok.reverse.1040@gmail.com>
2025-08-06 13:48:13 +0900

    feat(auth): hash user password before storing in database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from 'generated/prisma';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { name, email, password, status } = createUserDto;
+    const saltRounds = Number(process.env.BCRYPT_SALT_ROUNDS) || 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     return this.prismaService.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
         status,
       },
     });

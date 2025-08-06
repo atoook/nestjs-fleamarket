41cd71818c76b394234298c57c26f77cb6f652e0
Kota Kawamoto <atok.reverse.1040@gmail.com>
2025-08-06 15:19:26 +0900

    feat(auth): integrate Passport and JWT modules for authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from 'generated/prisma';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { CredentialsDto } from './dto/credentials.dto';
+import { jwtPayload } from './types/jwtPayload';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { name, email, password, status } = createUserDto;
     const saltRounds = Number(process.env.BCRYPT_SALT_ROUNDS) || 10;
@@ -19,5 +25,20 @@ export class AuthService {
         status,
       },
     });
+  }
+
+  async signIn(credentialsDto: CredentialsDto): Promise<{ token: string }> {
+    const { email, password } = credentialsDto;
+    const user = await this.prismaService.user.findUnique({ where: { email } });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payLoad: jwtPayload = {
+        sub: user.id,
+        username: user.name,
+        status: user.status,
+      };
+      const token = this.jwtService.sign(payLoad);
+      return { token };
+    }
+    throw new UnauthorizedException();
   }
 }

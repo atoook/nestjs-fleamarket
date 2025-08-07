0676c07654b52ee191fdedee4600b59042a071e8
Kota Kawamoto <atok.reverse.1040@gmail.com>
2025-08-07 16:12:46 +0900

    feat(main): enable CORS in the application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // DTOで定義されていないプロパティを自動で除去

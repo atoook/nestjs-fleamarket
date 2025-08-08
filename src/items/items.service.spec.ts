c95af1bd583e725cdf9b68564f28dfcba45b6a76
Kota Kawamoto <atok.reverse.1040@gmail.com>
2025-08-08 17:49:54 +0900

    test(items): add unit tests for create, updateStatus, and delete methods in ItemsService

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,14 +1,18 @@
 import { Test } from '@nestjs/testing';
 import { ItemsService } from './items.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { Item } from '../../generated/prisma';
+import { Item, ItemStatus } from '../../generated/prisma';
 import { NotFoundException } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 // Mock PrismaService for testing
 const mockPrismaService = {
   item: {
     findMany: jest.fn(),
     findUnique: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
   },
 };
 
@@ -26,6 +30,7 @@ describe('ItemsServiceTest', () => {
     itemsService = module.get<ItemsService>(ItemsService);
     prismaService = module.get<PrismaService>(PrismaService);
   });
+
   describe('findAll', () => {
     it('Normal', async () => {
       (prismaService.item.findMany as jest.Mock).mockResolvedValue([]);
@@ -34,6 +39,7 @@ describe('ItemsServiceTest', () => {
       expect(result).toEqual(expected);
     });
   });
+
   describe('findUnique', () => {
     it('Normal', async () => {
       const item: Item = {
@@ -60,6 +66,77 @@ describe('ItemsServiceTest', () => {
       await expect(itemsService.findById('test-id1')).rejects.toThrow(
         NotFoundException,
       );
+    });
+  });
+
+  describe('create', () => {
+    const createItemDto: CreateItemDto = {
+      name: 'PC',
+      price: 100000,
+      description: '未使用品',
+    };
+    const userId = 'test-user1';
+
+    const item: Item = {
+      id: 'test-id1',
+      name: createItemDto.name,
+      price: createItemDto.price,
+      description: createItemDto.description!,
+      status: ItemStatus.ON_SALE,
+      createdAt: new Date('2025-01-01'),
+      updatedAt: new Date('2025-01-01'),
+      userId,
+    };
+    it('Normal', async () => {
+      (prismaService.item.create as jest.Mock).mockResolvedValue(item);
+      const result = await itemsService.create(createItemDto, userId);
+      expect(result).toEqual(item);
+      expect(prismaService.item.create).toHaveBeenCalledWith({
+        data: {
+          ...createItemDto,
+          status: ItemStatus.ON_SALE,
+          userId,
+        },
+      });
+    });
+  });
+
+  describe('updateStatus', () => {
+    it('Normal', async () => {
+      const id: string = 'test-id1';
+      const userId: string = 'user-id1';
+      const item: Item = {
+        id,
+        name: 'test-item',
+        price: 10000,
+        description: 'test-description',
+        status: ItemStatus.SOLD_OUT,
+        createdAt: new Date('2025-01-01'),
+        updatedAt: new Date('2025-01-01'),
+        userId,
+      };
+      (prismaService.item.update as jest.Mock).mockResolvedValue(item);
+      const result = await itemsService.updateStatus(id);
+      expect(result).toEqual(item);
+      expect(prismaService.item.update).toHaveBeenCalledWith({
+        where: { id },
+        data: {
+          status: ItemStatus.SOLD_OUT,
+        },
+      });
+    });
+  });
+
+  describe('delete', () => {
+    it('Normal', async () => {
+      const id: string = 'test-id1';
+      const userId: string = 'test-user1';
+      (prismaService.item.delete as jest.Mock).mockResolvedValue(null);
+      const result = await itemsService.delete(id, userId);
+      expect(result).toEqual(undefined);
+      expect(prismaService.item.delete).toHaveBeenCalledWith({
+        where: { id, userId },
+      });
     });
   });
 });
